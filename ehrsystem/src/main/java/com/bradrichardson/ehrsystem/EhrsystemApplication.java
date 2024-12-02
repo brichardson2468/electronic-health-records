bef556cba445a41919700decacda15aba632c035
brichardson2468 <brichardson2468@gmail.com>
2024-12-02 15:05:45 -0500

    Initial Spring Boot project setup

---
+++ ehrsystem/src/main/java/com/bradrichardson/ehrsystem/EhrsystemApplication.java
@@ -0,0 +1,13 @@
+package com.bradrichardson.ehrsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhrsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhrsystemApplication.class, args);
+	}
+
+}
---
+++ ehrsystem/src/test/java/com/bradrichardson/ehrsystem/EhrsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bradrichardson.ehrsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhrsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
